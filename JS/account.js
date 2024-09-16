8e788ebd3f318954df212ce397d5f61edd9534da
Shamoil <139974413+shamoil-khan@users.noreply.github.com>
2024-09-16 18:43:36 +0500

    Update account.js

--- JS/account.js
+++ JS/account.js
@@ -6,12 +6,6 @@ import {
   child,
   get,
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
-// import {
-//   getFirestore,
-//   collection,
-//   addDoc,
-//   getDocs,
-// } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import {
   getAuth,
   signInWithPopup,
@@ -26,7 +20,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-// const db = getFirestore(app);
 const database = getDatabase(app);
 
 function writeUserData(userId, name, note, date, time, color) {
@@ -39,38 +32,6 @@ function writeUserData(userId, name, note, date, time, color) {
     color,
   });
 }
-
-// const dbRef = ref(getDatabase(app));
-// get(child(dbRef, `users/123ee`))
-//   .then((snapshot) => {
-//     if (snapshot.exists()) {
-//       console.log(snapshot.val());
-//     } else {
-//       console.log("No data available");
-//     }
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   });
-
-// !SITE KEY : 6Lc0l2spAAAAAK1sat013JXu1UmlfQpcFC-eCPyY
-// !SECRET KEY : 6Lc0l2spAAAAALi2hXBNo_tIDMTX47SyLfWtLT-Y
-
-// const user = auth.currentUser;
-
-// try {
-//   const docRef = await addDoc(collection(db, "users"), {
-//     exArr,
-//   });
-//   console.log("Document written with ID: ", docRef.id);
-// } catch (e) {
-//   console.error("Error adding document: ", e);
-// }
-
-// // const querySnapshot = await getDocs(collection(db, "users"));
-// // querySnapshot.forEach((doc) => {
-// //   console.log(`${doc.id} => ${JSON.parse(doc.data())}`);
-// // });
 
 const provider = new GoogleAuthProvider(app);
 
@@ -107,7 +68,6 @@ function googleFunction() {
       const displayName = user.displayName;
       const email = user.email;
       const photoURL = user.photoURL;
-      //   const emailVerified = user.emailVerified;
 
       profileContainer.classList.add("activated");
       profileImg.src = photoURL;
@@ -117,20 +77,12 @@ function googleFunction() {
     .catch((error) => {
       alertContainer.classList.add("active");
       showAlert(googleError);
-      //   const errorCode = error.code;
-      //   const errorMessage = error.message;
-      //   const email = error.customData.email;
-      //   const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
     });
 }
 
 function signOutFunc() {
   signOut(auth)
     .then(() => {
-      // Sign-out successful.
-      // console.log("Sign-out successful");
-
       alertContainer.classList.add("active");
       showAlert(signSuccess);
 
@@ -143,8 +95,6 @@ function signOutFunc() {
     .catch((error) => {
       alertContainer.classList.add("active");
       showAlert(signError);
-      // An error happened.
-      // console.log("An error happened.");
     });
 }
 
